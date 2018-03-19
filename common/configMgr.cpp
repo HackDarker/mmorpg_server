343bcfbd167cb07bc083ac86352bbd3e62b086e3
xiaofan <2309781817@qq.com>
2018-03-19 09:40:01 +0800

    aa

--- common/configMgr.cpp
+++ common/configMgr.cpp
@@ -1,32 +1,119 @@
-#ifndef CONFIGMGR_H
-#define CONFIGMGR_H
+#include "configMgr.h"
 
-#include <map>
-
-extern "C" 
+ConfigMgr::ConfigMgr()
 {
-	#include "lua.h"
-	#include "lualib.h"
-	#include "lauxlib.h"
-};
 
-class ConfigMgr
+}
+
+ConfigMgr::~ConfigMgr()
 {
-public:
-	~ConfigMgr();
-	static ConfigMgr* Instance()
-	{
-		static ConfigMgr inst;
-		return &inst;
+
+}
+
+bool ConfigMgr::LoadConfig()
+{
+	static const char * load_config = "\
+		local result = {}\n\
+		local function getenv(name) return assert(os.getenv(name), [[os.getenv() failed: ]] .. name) end\n\
+		local sep = package.config:sub(1,1)\n\
+		local current_path = [[.]]..sep\n\
+		local function include(filename)\n\
+			local last_path = current_path\n\
+			local path, name = filename:match([[(.*]]..sep..[[)(.*)$]])\n\
+			if path then\n\
+				if path:sub(1,1) == sep then	-- root\n\
+					current_path = path\n\
+				else\n\
+					current_path = current_path .. path\n\
+				end\n\
+			else\n\
+				name = filename\n\
+			end\n\
+			local f = assert(io.open(current_path .. name))\n\
+			local code = assert(f:read [[*a]])\n\
+			code = string.gsub(code, [[%$([%w_%d]+)]], getenv)\n\
+			f:close()\n\
+			assert(load(code,[[@]]..filename,[[t]],result))()\n\
+			current_path = last_path\n\
+		end\n\
+		setmetatable(result, { __index = { include = include } })\n\
+		local config_name = ...\n\
+		include(config_name)\n\
+		setmetatable(result, nil)\n\
+		return result\n\
+	";
+
+	struct lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
+	int err =  luaL_loadbufferx(L, load_config, strlen(load_config), "=[sever config]");
+	assert(err == LUA_OK);
+	lua_pushstring(L, config_file);
+
+	err = lua_pcall(L, 1, 1, 0);
+	if (err) {
+		fprintf(stderr,"%s\n",lua_tostring(L,-1));
+		lua_close(L);
+		return false;
 	}
 
-	bool LoadConfig(const char* filename);
-	bool GetBool(const char* key,int opt);
-	int  GetInt(const char* key,int opt);
-	const char* GetString(const char* key,const char* opt); 
-private:
-	ConfigMgr();
-	std::map<const char *key, const char *value> m_confMap;
-};
+	//set config key value
+	{
+		lua_pushnil(L);  /* first key */
+		while (lua_next(L, -2) != 0) {
+			int keyt = lua_type(L, -2);
+			if (keyt != LUA_TSTRING) {
+				fprintf(stderr, "Invalid config table\n");
+				exit(1);
+			}
+			const char * key = lua_tostring(L,-2);
+			if (lua_type(L,-1) == LUA_TBOOLEAN) {
+				int b = lua_toboolean(L,-1);
+				skynet_setenv(key,b ? "true" : "false" );
+			} else {
+				const char * value = lua_tostring(L,-1);
+				if (value == NULL) {
+					fprintf(stderr, "Invalid config table key = %s\n", key);
+					exit(1);
+				}
+				skynet_setenv(key,value);
+			}
+			lua_pop(L,1);
+		}
+		lua_pop(L,1);
+	}
 
-#endif
+	lua_close(L);
+
+	return true;
+}
+
+bool ConfigMgr::GetBool(const char* key,int opt)
+{
+	std::map<const char*, const char*>::iterator itr = m_confMap.find(key);
+	if(itr != m_confMap.end()){
+		return strcmp(str,"true")==0;
+	}
+
+	return opt;
+}
+
+int ConfigMgr::GetInt(const char* key,int opt)
+{
+	std::map<const char*, const char*>::iterator itr = m_confMap.find(key);
+	if(itr != m_confMap.end()){
+		return strtol(itr->second, NULL, 10);
+	}
+
+	return opt;
+}
+
+const char* ConfigMgr::GetString(const char* key,const char* opt)
+{
+	std::map<const char*, const char*>::iterator itr = m_confMap.find(key);
+	if(itr != m_confMap.end()){
+		return itr->second;
+	}
+
+	return opt;
+}

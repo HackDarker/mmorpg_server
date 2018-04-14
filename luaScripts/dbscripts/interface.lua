617c4cb89b9c096faa4d5886b58051524830add4
2309781817@qq.com <2309781817@qq.com>
2018-04-14 17:20:20 +0800

    aa

--- luaScripts/dbscripts/interface.lua
+++ luaScripts/dbscripts/interface.lua
@@ -4,32 +4,10 @@ local dbOpCodes = dbOpCodes
 local dbPacketHandler = dbPacketHandler
 local msgpack  = msgpack
 
- pb.register_file('/home/source2222/proto/built/db.pb')
- print('registered file','/home/source2222/proto/built/db.pb')
-
-
 -----------------------------------------------------------------------------------------------------------------------------
 --处理查询包
 function c_procQuery(accountId, charId, pack)
-	print("c_procQuery===========enter============11111",accountId,charId,pack)
-	print("c_procQuery===========22222222",msgpack.getOpcode(pack))
-
-	local pbMsg,pbSize = msgpack.getContents(pack)
-
-	print("c_procQuery333333333333333333",pbMsg,pbSize,pb.decode)
-
-	local pbname = 'db.L2D_ROLE_LIST'
-    local req_msg = pb.decode(pbname,pbMsg,pbSize)
-    if not req_msg then
-        print("c_procQuery pb decode error",pbname)
-        return
-    end
-
-    for k,v in pairs(req_msg) do
-    	print("ttttttttttttttttttt===",k,v,type(v))
-    end
-
-    print("ccccccccccccccccccccccccccc_procQuery",req_msg.account_id)
+	
 end
 
 -----------------------------------------------------------------------------------------------------------------------------
@@ -38,4 +16,4 @@ function c_procExec(accountId, charId, pack)
     print("c_procExec===========",accountId,charId)
 end
 
-print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",msgpack,pb)
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxaaaa",msgpack,msgpack.getSize)
